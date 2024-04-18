f969afed51e80e2a3ff43dcc45bdc30c7f562d41
shrewman <feelswhiteman@gmail.com>
2024-04-18 20:34:04 +0300

    implement recorder

--- src/components/Recorder.tsx
+++ src/components/Recorder.tsx
@@ -1,3 +1,28 @@
+import { useState } from "react";
+import { AudioRecorder } from "react-audio-voice-recorder";
+
 export default function Recorder() {
-  return <></>;
+  const [audioUrl, setAudioUrl] = useState<string | null>(null);
+
+  const addAudioElement = (blob: Blob) => {
+    const url = URL.createObjectURL(blob);
+    setAudioUrl(url);
+  };
+
+  return (
+    <>
+      <AudioRecorder
+        onRecordingComplete={addAudioElement}
+        audioTrackConstraints={{
+          noiseSuppression: false,
+          echoCancellation: false,
+        }}
+        downloadOnSavePress={false}
+        downloadFileExtension="webm"
+        showVisualizer={true}
+      />
+      <br />
+      {audioUrl && <audio src={audioUrl} controls />}
+    </>
+  );
 }
