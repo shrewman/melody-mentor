1fe8dd5252224f0ac8f8868345d0dd8f0ee20a57
shrewman <feelswhiteman@gmail.com>
2024-04-22 22:45:43 +0300

    fetch data about songs from server and display it

--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -1,3 +1,43 @@
+import { useEffect, useState } from "react";
+
+type Song = {
+  song_id: number;
+  title: string;
+  artist: string;
+  duration_seconds: number;
+  file_location: string;
+};
+
 export default function Songs() {
-  return <></>;
+  const [songs, setSongs] = useState<Song[] | null>(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/api/songs")
+      .then((res) => {
+        if (!res.ok) {
+          alert("not ok");
+          return;
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setSongs(data);
+      })
+      .catch((err) => {
+        console.error("Error fetching songs: ", err);
+      });
+  }, []);
+
+  return (
+    <>
+      <div>
+        {songs?.map((song) => (
+          <div key={song.song_id}>
+            <div>{song.title}</div>
+            <div>{song.artist}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
