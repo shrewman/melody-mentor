cc51fde6e5bfd772bfcb885a5dd347582b1dec33
shrewman <feelswhiteman@gmail.com>
2024-04-22 22:43:14 +0300

    feat: create server that serves info about songs

---
+++ server/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import cors from "cors";
+import mysql from "mysql";
+import "dotenv/config";
+
+const app = express();
+const PORT = 3000;
+
+console.log(process.env.DB_NAME);
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to db: " + err.stack);
+    return;
+  }
+  console.log("Connected to db");
+});
+
+app.use("/songs", express.static("songs"));
+app.use(cors());
+
+app.get("/api/songs", (req, res) => {
+  const sql = "SELECT * FROM songs";
+  connection.query(sql, (err, results) => {
+    if (err) {
+      console.error("Error retrieving songs: " + err);
+      res.status(500).json({ error: "Internal server error" });
+      return;
+    }
+    console.log(results);
+    res.json(results);
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
