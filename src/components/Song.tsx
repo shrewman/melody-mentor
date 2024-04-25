2ceb46edeaf7048ea4f6348dbdd224087b213826
shrewman <feelswhiteman@gmail.com>
2024-04-25 06:29:52 +0300

    refactor Songs component

---
+++ src/components/Song.tsx
@@ -0,0 +1,29 @@
+import { Song as SongType} from "../types";
+import ComplexityBar from "./ComplexityBar";
+
+type Props = {
+  song: SongType;
+};
+
+const Song: React.FC<Props> = ({ song }) => {
+  const convertDuration = (seconds: number) => {
+    const min = Math.floor(seconds / 60);
+    const sec = seconds % 60;
+    const fmin = String(min).padStart(2, "0");
+    const fsec = String(sec).padStart(2, "0");
+    return `${fmin}:${fsec}`;
+  };
+
+  return (
+    <div className="mb-4 flex justify-between border-4 border-surface1 bg-surface0 p-3">
+      <div>
+        <h1 className="mb-3 font-bold">{song.title}</h1>
+        <h2 className="mb-3">{song.artist}</h2>
+        <p>{convertDuration(song.duration_seconds)}</p>
+      </div>
+      <ComplexityBar complexity={song.complexity} />
+    </div>
+  );
+};
+
+export default Song;
--- src/components/Songs.tsx
+++ src/components/Songs.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-import { Song } from "../types";
-import ComplexityBar from "./ComplexityBar";
+import { Song as SongType } from "../types";
+import Song from "./Song";
 
 export default function Songs() {
-  const [songs, setSongs] = useState<Song[] | null>(null);
+  const [songs, setSongs] = useState<SongType[] | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -24,33 +24,13 @@ export default function Songs() {
       });
   }, []);
 
-  const convertDuration = (seconds: number) => {
-    const min = Math.floor(seconds / 60);
-    const sec = seconds % 60;
-    const fmin = String(min).padStart(2, "0");
-    const fsec = String(sec).padStart(2, "0");
-    return `${fmin}:${fsec}`;
-  };
-
   return (
     <>
       {isLoading ? (
         <div>loading...</div>
       ) : (
         <div className="w-full  p-3">
-          {songs?.map((song) => (
-            <div
-              key={song.song_id}
-              className="mb-4 flex justify-between border-4 border-surface1 bg-surface0 p-3"
-            >
-              <div>
-                <h1 className="mb-3 font-bold">{song.title}</h1>
-                <h2 className="mb-3">{song.artist}</h2>
-                <p>{convertDuration(song.duration_seconds)}</p>
-              </div>
-              <ComplexityBar complexity={song.complexity} />
-            </div>
-          ))}
+          {songs?.map((song) => <Song song={song} />)}
         </div>
       )}
     </>
