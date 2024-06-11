5e75efeda31e2262d6cd8d6974a28dfd63b8e507
shrewman <feelswhiteman@gmail.com>
2024-06-11 12:52:21 +0300

    style the recorder page

--- src/components/RecorderPage.tsx
+++ src/components/RecorderPage.tsx
@@ -13,18 +13,28 @@ export default function RecorderPage() {
   return (
     <>
       <Navbar />
-      <AudioRecorder
-        onRecordingComplete={addAudioElement}
-        audioTrackConstraints={{
-          noiseSuppression: false,
-          echoCancellation: false,
-        }}
-        downloadOnSavePress={false}
-        downloadFileExtension="webm"
-        showVisualizer={true}
-      />
-      <br />
-      {audioUrl && <audio src={audioUrl} controls />}
+      <div className="m-5">
+        <h1 className="mb-5 text-center">Recorder</h1>
+        <h2 className="mb-5 text-center">
+          Press the button to start recording
+        </h2>
+        <div className="flex justify-center">
+          <AudioRecorder
+            onRecordingComplete={addAudioElement}
+            audioTrackConstraints={{
+              noiseSuppression: false,
+              echoCancellation: false,
+            }}
+            downloadOnSavePress={false}
+            downloadFileExtension="webm"
+            showVisualizer={true}
+          />
+        </div>
+        <br />
+        <div className="flex justify-center">
+          {audioUrl && <audio src={audioUrl} controls className="w-1/2" />}
+        </div>
+      </div>
     </>
   );
 }
