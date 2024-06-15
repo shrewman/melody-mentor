7869205bc472b04fe0e9333ed92e49b559ae4b65
shrewman <feelswhiteman@gmail.com>
2024-06-15 14:01:41 +0300

    restyle tuner

--- src/components/TunerPage.tsx
+++ src/components/TunerPage.tsx
@@ -1,12 +1,21 @@
 import { useState, useEffect } from "react";
-import noteFrequencyMap from "../utils/noteFrequencyMap.ts";
-import Navbar from "./Navbar.tsx";
+import noteFrequencyMap from "../utils/noteFrequencyMap";
+import Navbar from "./Navbar";
+
+type Note = keyof typeof noteFrequencyMap;
 
 const TunerPage = () => {
   const [isTuning, setIsTuning] = useState(false);
   const [detectedFrequency, setDetectedFrequency] = useState(0);
   const [stream, setStream] = useState<MediaStream | null>(null);
-  const [nearestNote, setNearestNote] = useState<string | null>(null);
+  const [nearestNote, setNearestNote] = useState<Note>("C0");
+  const [previousNote, setPreviousNote] = useState<Note>("B0");
+  const [nextNote, setNextNote] = useState<Note>("C#0");
+  const [tuningProgress, setTuningProgress] = useState(0);
+
+  const notes = (Object.keys(noteFrequencyMap) as Note[]).sort(
+    (a, b) => noteFrequencyMap[a] - noteFrequencyMap[b],
+  );
 
   useEffect(() => {
     const audioContext = new AudioContext();
@@ -16,8 +25,6 @@ const TunerPage = () => {
           audio: true,
         });
         setStream(micStream);
-        // Bigger the number - more precise result and more time to compute
-        // const fftSize = 8192;
         const fftSize = 16384;
         const analyser = audioContext.createAnalyser();
         analyser.fftSize = fftSize;
@@ -61,29 +68,81 @@ const TunerPage = () => {
     }
   }, [isTuning]);
 
-  // TODO: find faster solution
   useEffect(() => {
     let minDifference = Number.MAX_VALUE;
+    let closestNote = "C" as Note;
+    let closestFrequency = 0;
 
     for (const [note, frequency] of Object.entries(noteFrequencyMap)) {
       const difference = Math.abs(detectedFrequency - frequency);
       if (difference < minDifference) {
         minDifference = difference;
-        setNearestNote(note);
+        closestNote = note as Note;
+        closestFrequency = frequency;
       }
     }
 
-  }, [detectedFrequency]);
+    setNearestNote(closestNote);
 
-  // TODO: better interface, <progress> maybe? 
+    if (closestFrequency !== 0) {
+      const noteIndex = notes.indexOf(closestNote);
+      const previousIndex = (noteIndex - 1 + notes.length) % notes.length;
+      const nextIndex = (noteIndex + 1) % notes.length;
+      setPreviousNote(notes[previousIndex]);
+      setNextNote(notes[nextIndex]);
+
+      const previousFrequency = noteFrequencyMap[notes[previousIndex]];
+      const nextFrequency = noteFrequencyMap[notes[nextIndex]];
+
+      const progress =
+        ((detectedFrequency - previousFrequency) /
+          (nextFrequency - previousFrequency)) *
+        100;
+      setTuningProgress(progress);
+    } else {
+      setTuningProgress(0);
+      setPreviousNote("B0");
+      setNextNote("C#0");
+    }
+  }, [detectedFrequency, notes]);
+
   return (
     <>
       <Navbar />
-      <button onClick={() => setIsTuning(!isTuning)}>
-        {isTuning ? "Stop Tuning" : "Start Tuning"}
-      </button>
-      <p>Detected Frequency: {detectedFrequency} Hz</p>
-      <p>Nearest Note: {nearestNote}</p>
+      <div className="bg-surface0 rounded-xl p-3 mt-5 w-1/2 mx-auto">
+        <div className="mx-auto w-2/3 text-center">
+          <button
+            className="rounded bg-blue px-4 py-2 text-surface0"
+            onClick={() => setIsTuning(!isTuning)}
+          >
+            {isTuning ? "Stop Tuning" : "Start Tuning"}
+          </button>
+          <p className="mt-5">
+            Detected Frequency: {detectedFrequency.toFixed(2)} Hz
+          </p>
+          <div className="mt-3">
+            <div className="relative pt-1">
+              <div className="mb-2 grid grid-cols-9 text-gray-600">
+                <span className="mt-5 text-xl text-text">
+                  {previousNote.slice(0, -1)}
+                </span>
+                <span className="text-4xl text-white col-span-7">
+                  {nearestNote.slice(0, -1)}
+                </span>
+                <span className="mt-5 text-xl text-text">
+                  {nextNote.slice(0, -1)}
+                </span>
+              </div>
+              <div className="mb-4 flex h-2 overflow-hidden rounded bg-surface2 text-xs">
+                <div
+                  style={{ width: `${tuningProgress}%` }}
+                  className="flex flex-col justify-center whitespace-nowrap bg-blue text-center"
+                ></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
