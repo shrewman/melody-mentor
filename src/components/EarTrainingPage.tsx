6c990568a0a6048fe7728d3b7e8d20333eefabaf
shrewman <feelswhiteman@gmail.com>
2024-06-25 10:11:14 +0300

    update style for EarTrainingPage

--- src/components/EarTrainingPage.tsx
+++ src/components/EarTrainingPage.tsx
@@ -72,9 +72,9 @@ const EarTrainingPage: React.FC = () => {
   return (
     <>
       <Navbar />
-      <div className="flex justify-center">
-        <div className="m-5 flex w-1/2 flex-col items-center justify-center rounded-xl bg-surface0">
-          <div className="m-5 h-8 text-xl font-bold text-text">{message}</div>
+      <div className="conatiner mx-auto max-w-[800px] ">
+        <div className="mx-auto flex flex-col m-5 items-center justify-center rounded-xl bg-surface0">
+          <div className="my-3 m-5 h-8 text-xl font-bold text-text">{message}</div>
           <div className="text-2xl font-bold text-text">
             First Note: {firstNote.slice(0, -1)}
           </div>
@@ -82,7 +82,7 @@ const EarTrainingPage: React.FC = () => {
             {notes.map((note) => (
               <button
                 key={note}
-                className={`rounded px-4 py-2 text-xl font-bold text-surface1 ${isDisabled ? "bg-surface2" : "bg-blue"}`}
+                className={`m-1 rounded px-4 py-2 text-xl font-bold text-surface1 ${isDisabled ? "bg-surface2" : "bg-blue"}`}
                 disabled={isDisabled}
                 onClick={() => handleGuess(note)}
               >
@@ -90,16 +90,16 @@ const EarTrainingPage: React.FC = () => {
               </button>
             ))}
           </div>
-          <div className="grid grid-cols-2 p-2">
+          <div className="my-3 grid gap-5 p-2 md:grid-cols-2">
             <button
-              className={`mt-4 rounded px-4 py-2 font-bold text-surface1 ${isDisabled ? "bg-surface2" : "bg-lavender"}`}
+              className={`p-2 font-bold text-surface1 ${isDisabled ? "bg-surface2" : "bg-lavender"}`}
               onClick={handleNewGame}
               disabled={isDisabled}
             >
               New Game
             </button>
             <button
-              className={`mt-4 rounded px-4 py-2 font-bold text-surface1 ${isDisabled ? "bg-surface2" : "bg-green"}`}
+              className={`p-2 font-bold text-surface1 ${isDisabled ? "bg-surface2" : "bg-green"}`}
               onClick={handleReplayNotes}
               disabled={isDisabled}
             >
